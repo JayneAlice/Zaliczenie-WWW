0f11243647b6abbf66982f85f783320e924e82d3
JayneAlice <amatuszak5@st.swps.edu.pl>
2024-10-28 10:06:47 +0100

    Recreate

--- AplikacjaZaliczeniowa/AplikacjaZaliczeniowa/urls.py
+++ AplikacjaZaliczeniowa/AplikacjaZaliczeniowa/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AplikacajZaliczeniowa project.
+URL configuration for AplikacjaZaliczeniowa project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
